d7d825724582878256bf0a30667c63d4966d90f4
sinsu <sinsumathew93@gmail.com>
2018-02-19 14:38:33 +0400

    Added the add method

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -8,5 +8,12 @@ namespace ConsoleApp1
 {
     class Feature1
     {
+        public int Add()
+        {
+            int a = 1;
+            int b = 2;
+            int sum = a + b;
+            return sum;
+        }
     }
 }
