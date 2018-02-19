2c382399f900173b0eca74fb5ae7dcc4ad397bf5
sinsu <sinsumathew93@gmail.com>
2018-02-19 14:43:37 +0400

    Edited the code - refactor

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -8,12 +8,10 @@ namespace ConsoleApp1
 {
     class Feature1
     {
-        public int Add()
+        public int Add(int a, int b)
         {
-            int a = 1;
-            int b = 2;
-            int sum = a + b;
-            return sum;
+          
+            return a+b;
         }
     }
 }
